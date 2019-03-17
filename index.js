d07998b8f3fc84606791ebcd746b2c5801941fa2
Ay Has <ahashim983@yahoo.com>
2019-03-17 11:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var height = "74"
 
 if (name == "Joe") {
   console.log("The name is correct")
+} else {
+  console.log("Expected name to be Joe")
 }
 // Don't worry about this
 module.exports = { name, height
